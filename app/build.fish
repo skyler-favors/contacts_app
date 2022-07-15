b15611cc63228df3502df43787770c27579c6df1
sky-86 <sky86one@gmail.com>
2022-07-14 22:33:19 -0500

    added build script

---
+++ app/build.fish
@@ -0,0 +1,52 @@
+#!/opt/homebrew/bin/fish
+
+function build_debug
+  tailwindcss -i tailwind/tailwind.css -o tailwind/out/tailwind.css
+  trunk build 
+end
+
+function build_release
+  tailwindcss -i tailwind/tailwind.css -o tailwind/out/tailwind.css --minify
+  trunk build --release
+end
+
+function serve_debug
+  build_debug
+  cd ../server
+  cargo run
+end
+
+function serve_release
+  build_release
+  cd ../server
+  cargo run --release
+end
+
+function help
+  echo "-sd --serve_debug"
+  echo "-sr --serve_release"
+  echo "-d --build_debug"
+  echo "-r --build_release"
+end
+
+if test -z $argv
+  help
+end
+
+for arg in $argv
+  switch $arg
+    case -sd --serve_debug
+      serve_debug
+    case -sr --serve_release
+      serve_release
+    case -d --debug
+      build_debug
+    case -r --release
+      build_release
+    case -h --help; or '*'
+      help
+  end
+end
+
+
+
